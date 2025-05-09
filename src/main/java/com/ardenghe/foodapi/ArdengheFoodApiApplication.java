cf35ba7b6fb82478d7888dfeb39e88312024903a
Giovanni Ardenghe <ardenghegiovanni@gmail.com>
2025-05-09 10:07:56 -0300

    First Commit

---
+++ src/main/java/com/ardenghe/foodapi/ArdengheFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.ardenghe.foodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArdengheFoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArdengheFoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ardenghe/foodapi/ArdengheFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardenghe.foodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArdengheFoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
